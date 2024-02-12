0e1219de91ca1dcf6aa24b0f8d5c798220769e6f
devw45 <devin.wingfield45@gmail.com>
2024-02-12 00:06:04 -0600

    Beginning of REACT tutorial

---
+++ React Tutorial/react-app/src/App.tsx
@@ -0,0 +1,10 @@
+// import Message from './Message'
+
+function App() {
+  const name = "Devin";
+  if (name)
+    return <h1>Hello {name}</h1> //same thing as <message></message>
+  return <h1>Hello World</h1> //same thing as <message></message>
+}
+
+export default App;
